b820623f87184440c6d5e8793c2162df5162e287
Ranjith Kumar T <Ranjitht@hns.com>
2018-03-07 16:45:32 +0530

    upload all files commit

---
+++ vr/client.js
@@ -0,0 +1,39 @@
+/**
+ * The examples provided by Oculus are for non-commercial testing and
+ * evaluation purposes only.
+ *
+ * Oculus reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL
+ * OCULUS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Example ReactVR app that allows a simple tour using linked 360 photos.
+ */
+import * as OVRUI from 'ovrui';
+import ControllerRayCaster from 'react-vr-controller-raycaster';
+import {VRInstance} from 'react-vr-web';
+
+function init(bundle, parent, options) {
+  const vr = new VRInstance(bundle, 'TourSample', parent, {
+    
+    // specify your list of raycasters
+    raycasters: [
+      new ControllerRayCaster({scene, color: '#ff0000'}),
+      new OVRUI.MouseRayCaster(),
+    ],
+    cursorVisibility: 'visible',// Show a gaze cursor.
+    ...options,
+  });
+  vr.render = function() {
+    // Any custom behavior you want to perform on each frame goes here
+  };
+  // Begin the animation loop
+  vr.start();
+  return vr;
+}
+
+window.ReactVR = {init};
